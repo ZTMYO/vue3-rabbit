1ed4284607871c5b84de3fce94c82cae9ec0af9b
ZTMYO <675253047@qq.com>
2025-07-20 16:43:32 +0800

    项目路由设计

---
+++ src/views/Category/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>我是分类页</div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="layout-container">
+    <nav class="navbar">
+      <!-- 导航链接：跳转到首页 -->
+      <RouterLink to="/">首页</RouterLink>
+      
+      <!-- 导航链接：跳转到分类页 -->
+      <RouterLink to="/category">分类</RouterLink>
+      
+      <!-- 导航链接：跳转到登录页 -->
+      <RouterLink to="/login">登录</RouterLink>
+    </nav>
+    我是首页
+    <!-- 二级路由出口 -->
+    <RouterView />
+  </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
