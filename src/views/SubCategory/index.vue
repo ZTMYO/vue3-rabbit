d7380d8ee52e5c850f2294f1f4856a72765b0a86
ZTMYO <675253047@qq.com>
2025-07-21 21:45:15 +0800

    二级分类-列表无限加载实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -27,10 +27,24 @@ const getGoodList = async () => {
 onMounted(() => getGoodList())
 
 //tab切换回调
-const tabChange = ()=>{
+const tabChange = () => {
     console.log(reqData.value.sortField)
     reqData.value.page = 1
     getGoodList()
+}
+
+//加载更多
+const disabled = ref(false)
+const load = async() => {
+    //获取下一页数据
+    reqData.value.page++
+    const res = await getSubCategoryAPI(reqData.value)
+    //新老数据合并
+    goodList.value = [...goodList.value,...res.result.items]
+    //加载完毕 停止监听
+    if(res.result.length === 0){
+        disabled.value = true
+    }
 }
 </script>
 
@@ -52,10 +66,9 @@ const tabChange = ()=>{
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
+            <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
                 <!-- 商品列表-->
                 <GoodsItem v-for="good in goodList" :good="good" :key="good.id" />
-
             </div>
         </div>
     </div>
