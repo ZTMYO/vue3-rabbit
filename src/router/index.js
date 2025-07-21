506dfea33ccce6ce52403632724d6e61d06b9814
ZTMYO <675253047@qq.com>
2025-07-21 21:06:14 +0800

    二级分类-面包屑导航实现

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,9 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   // path和component的映射关系
@@ -21,6 +24,10 @@ const router = createRouter({
         {
           path: 'category/:id',
           component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
         }
       ]
     },
