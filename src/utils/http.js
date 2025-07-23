92682dfac76c2edb3d0397336b5303ffd9e308dd
ZTMYO <675253047@qq.com>
2025-07-23 21:48:24 +0800

    401token失效拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 import 'element-plus/theme-chalk/el-message.css'
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -27,6 +28,14 @@ httpInstance.interceptors.response.use(res => res.data, e => {
         message: e.response.data.message,
         type: 'error'
     })
+    // 401token失效处理
+    // 清除本地用户数据,跳转到登陆页
+    if (e.response.status === 401) {
+        const userStore = useUserStore()
+        userStore.clearUserInfo()
+        router.push('/login')
+    }
+    
     return Promise.reject(e)
 })
 
