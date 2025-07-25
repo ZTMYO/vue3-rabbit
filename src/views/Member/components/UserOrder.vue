d525ac373c3cf9db6d84a2fbe0459b89cfba4d6a
ZTMYO <675253047@qq.com>
2025-07-25 11:08:24 +0800

    订单-使用tab切换获取状态数据

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -14,25 +14,30 @@ const tabTypes = [
 
 const orderList = ref([])
 
-const params = {
+const params = ref({
   orderState:0,
   page:1,
   pageSize:2
-}
+})
 const getUserOrderList = async () => { 
-  const res = await getUserOrder(params)
+  const res = await getUserOrder(params.value)
   orderList.value = res.result.items
 }
 onMounted(()=>{
   getUserOrderList()
 })
 
+//  tab切换
+const tabChange = (type) => { 
+  params.value.orderState = type
+  getUserOrderList()
+}
 
 </script>
 
 <template>
   <div class="order-container">
-    <el-tabs>
+    <el-tabs @tab-change="tabChange">
       <!-- tab切换 -->
       <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
 
